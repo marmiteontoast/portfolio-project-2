ed9033b8abf330b919fc0a6b9b216d12af6fb6dd
mark john <mark.john@editmatters.co.uk>
2022-04-04 18:42:00 +0000

    add to javascript page created function compRandomAnswer.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const buttons = document.querySelectorAll('#btn-selector')
-const user = document.querySelector('#user')
-const comp = document.querySelector('#comp')
-const userScore = document.querySelector('#user-score')
-const compScore = document.querySelector('#comp-score')
+const playerPicksDisplay = document.querySelector('#player-picks')
+const compPicksDisplay= document.querySelector('#comp-picks')
+const resultDisplay = document.querySelector('#result')
 
+const button = document.querySelectorAll('button')
+let playerPicks
+let compPicks
 
-function mainGame() {
-    for (button of buttons) 
-    addEventListener('click', mainGame())
+button.forEach(button => button.addEventListener('click', (e) => {
+    playerPicks = e.target.id
+    playerPicksDisplay.innerHTML = playerPicks
+    compRandomAnswer()
+}))
 
-    
+function compRandomAnswer() {
+    const randomNumber = Math.floor(Math.random() * button.length + 1)
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        playerPicks = 'rock'
+    } else if (randomNumber === 2) {
+        compPicks = 'scissors'
+    } else if (randomNumber === 3) {
+        compPicks = 'paper'
+    }
+
+    compPicksDisplay.innerHTML = compPicks
 }
