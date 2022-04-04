2ab3ee798d539470330016d642093bed7ecc17cd
mark john <mark.john@editmatters.co.uk>
2022-04-04 15:50:23 +0000

    edit html page. changed id names

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('im here')
+const buttons = document.querySelectorAll('#btn-selector')
+const user = document.querySelector('#user')
+const comp = document.querySelector('#comp')
+const userScore = document.querySelector('#user-score')
+const compScore = document.querySelector('#comp-score')
+
+
+function mainGame() {
+    for (button of buttons) 
+    addEventListener('click', mainGame())
+
+    
+}
