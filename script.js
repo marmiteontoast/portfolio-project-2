a2becd3a5fd23a2c930e842d539551989f18ab21
mark john <mark.john@editmatters.co.uk>
2022-04-06 02:38:42 +0000

    add getResult function to javascript page

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const resultDisplay = document.querySelector('#result')
 const button = document.querySelectorAll('button')
 let playerPicks
 let compPicks
+let result 
 
 button.forEach(button => button.addEventListener('click', (e) => {
     playerPicks = e.target.id
     playerPicksDisplay.innerHTML = playerPicks
     compRandomAnswer()
+    getResult()
 }))
 
 function compRandomAnswer() {
@@ -17,7 +19,7 @@ function compRandomAnswer() {
     console.log(randomNumber)
 
     if (randomNumber === 1) {
-        playerPicks = 'rock'
+        compPicks = 'rock'
     } else if (randomNumber === 2) {
         compPicks = 'scissors'
     } else if (randomNumber === 3) {
@@ -25,4 +27,24 @@ function compRandomAnswer() {
     }
 
     compPicksDisplay.innerHTML = compPicks
+}
+
+function getResult() {
+    if (compPicks === playerPicks) {
+        result = 'its a draw!'
+    } else if (compPicks === 'rock' && playerPicks === "paper") {
+        result = 'you win'
+    } else if (compPicks === 'rock' && playerPicks === "scissors") {
+        result = 'you lose'
+    } else if (compPicks === 'paper' && playerPicks === "scissors") {
+        result = 'you win'
+    } else if (compPicks === 'paper' && playerPicks === "rock") {
+        result = 'you lose' 
+    } else if (compPicks === 'scissors' && playerPicks === "rock") {
+        result = 'you win'
+    } else if (compPicks === 'scissors' && playerPicks === "paper") {
+        result = 'you lose'
+    }
+
+    resultDisplay.innerHTML = result
 }
