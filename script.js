4041f45d891d41b6e20b465b1f2b57a64ea1645d
mark john <mark.john@editmatters.co.uk>
2022-04-10 14:28:42 +0000

    modified html page and started css styling

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const playerPicksDisplay = document.querySelector('#player-picks')
 const compPicksDisplay= document.querySelector('#comp-picks')
 const resultDisplay = document.querySelector('#result')
+const playerScore = document.querySelector('#player-score')
+const compScore = document.querySelector('#comp-score')
 
 const button = document.querySelectorAll('button')
 let playerPicks
 let compPicks
 let result 
+let userScore = 0
+let machineScore = 0
 
 button.forEach(button => button.addEventListener('click', (e) => {
     playerPicks = e.target.id
     playerPicksDisplay.innerHTML = playerPicks
     compRandomAnswer()
     getResult()
+    gameOver()
 }))
 
 function compRandomAnswer() {
@@ -24,7 +29,7 @@ function compRandomAnswer() {
         compPicks = 'scissors'
     } else if (randomNumber === 3) {
         compPicks = 'paper'
-    }
+    };
 
     compPicksDisplay.innerHTML = compPicks
 }
@@ -34,17 +39,42 @@ function getResult() {
         result = 'its a draw!'
     } else if (compPicks === 'rock' && playerPicks === "paper") {
         result = 'you win'
+        userScore++;
+        playerScore.textContent = userScore;
+
     } else if (compPicks === 'rock' && playerPicks === "scissors") {
         result = 'you lose'
+        machineScore++;
+        compScore.textContent = machineScore;
     } else if (compPicks === 'paper' && playerPicks === "scissors") {
         result = 'you win'
+        userScore++;
+        playerScore.textContent = userScore;
     } else if (compPicks === 'paper' && playerPicks === "rock") {
-        result = 'you lose' 
+        result = 'you lose'
+        machineScore++;
+        compScore.textContent = machineScore; 
     } else if (compPicks === 'scissors' && playerPicks === "rock") {
         result = 'you win'
+        userScore++;
+        playerScore.textContent = userScore;
     } else if (compPicks === 'scissors' && playerPicks === "paper") {
         result = 'you lose'
-    }
+        machineScore++;
+        compScore.textContent = machineScore;
+    };
 
     resultDisplay.innerHTML = result
+     
 }
+
+function gameOver(getResult) {
+    if (userScore === 6) {
+        alert('player has won the match')
+        location.reload()
+    } else if (machineScore === 6) {
+        alert('computer has won the match')
+        location.reload()
+    } 
+    
+} 
