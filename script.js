9e820941621058d1bf9f561f36fc8526edefc054
mark john <mark.john@editmatters.co.uk>
2022-04-17 14:36:18 +0000

    further improvements to javascript page

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
+/*jshint esversion: 6 */
 // declared variables
-const playerPicksDisplay = document.querySelector('#player-picks')
-const compPicksDisplay= document.querySelector('#comp-picks')
-const resultDisplay = document.querySelector('#result')
-const playerScore = document.querySelector('#player-score')
-const compScore = document.querySelector('#comp-score')
+const playerPicksDisplay = document.querySelector('#player-picks');
+const compPicksDisplay= document.querySelector('#comp-picks');
+const resultDisplay = document.querySelector('#result');
+const playerScore = document.querySelector('#player-score');
+const compScore = document.querySelector('#comp-score');
 
-const button = document.querySelectorAll('button')
-let playerPicks
-let compPicks
-let result 
-let userScore = 0
-let machineScore = 0
+const button = document.querySelectorAll('button');
+let playerPicks;
+let compPicks;
+let result; 
+let userScore = 0;
+let machineScore = 0;
 
 /**
  * main game function
  */
 button.forEach(button => button.addEventListener('click', function() {
-    playerPicks = this.id
-    playerPicksDisplay.innerHTML = playerPicks
-    compRandomAnswer()
-    getResult()
-    gameOver()
-    // reStart()
-}))
+    playerPicks = this.id;
+    playerPicksDisplay.innerHTML = playerPicks;
+    compRandomAnswer();
+    getResult();
+    gameOver();
+    reStart();
+}));
 
 /**
  * fuction generates ranodom number
  */
 function compRandomAnswer() {
-    const randomNumber = Math.floor(Math.random() * button.length + 1)
-    console.log(randomNumber)
+    const randomNumber = Math.floor(Math.random() * button.length + 1);
+    console.log(randomNumber);
 
     if (randomNumber === 1) {
-        compPicks = 'rock'
+        compPicks = 'rock';
     } else if (randomNumber === 2) {
-        compPicks = 'scissors'
+        compPicks = 'scissors';
     } else if (randomNumber === 3) {
-        compPicks = 'paper'
-    };
+        compPicks = 'paper';
+    }
 
-    compPicksDisplay.innerHTML = compPicks
+    compPicksDisplay.innerHTML = compPicks;
 }
 
 /**
@@ -47,35 +48,35 @@ function compRandomAnswer() {
  */
 function getResult() {
     if (compPicks === playerPicks) {
-        result = 'its a draw!'
+        result = 'its a draw!';
     } else if (compPicks === 'rock' && playerPicks === "paper") {
-        result = 'you win'
+        result = 'you win';
         userScore++;
         playerScore.textContent = userScore;
 
     } else if (compPicks === 'rock' && playerPicks === "scissors") {
-        result = 'you lose'
+        result = 'you lose';
         machineScore++;
         compScore.textContent = machineScore;
     } else if (compPicks === 'paper' && playerPicks === "scissors") {
-        result = 'you win'
+        result = 'you win';
         userScore++;
         playerScore.textContent = userScore;
     } else if (compPicks === 'paper' && playerPicks === "rock") {
-        result = 'you lose'
+        result = 'you lose';
         machineScore++;
         compScore.textContent = machineScore; 
     } else if (compPicks === 'scissors' && playerPicks === "rock") {
-        result = 'you win'
+        result = 'you win';
         userScore++;
         playerScore.textContent = userScore;
     } else if (compPicks === 'scissors' && playerPicks === "paper") {
-        result = 'you lose'
+        result = 'you lose';
         machineScore++;
         compScore.textContent = machineScore;
-    };
+    }
 
-    resultDisplay.innerHTML = result
+    resultDisplay.innerHTML = result;
      
 }
 
@@ -84,11 +85,11 @@ function getResult() {
  */
 function gameOver(getResult) {
     if (userScore === 6) {
-        alert('player has won the match')
-        location.reload()
+        alert('player has won the match');
+        location.reload();
     } else if (machineScore === 6) {
-        alert('computer has won the match')
-        location.reload()
+        alert('computer has won the match');
+        location.reload();
     } 
     
 } 
@@ -98,7 +99,7 @@ function gameOver(getResult) {
  */
 function reStart() {
    const reStart = document.querySelector('#reset').addEventListener('click', function() {
-        window.location.reload()
-   })
+        window.location.reload();
+   });
   
 }
